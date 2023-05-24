d1366a3cf1569760eaf7ffb466782aa8ed0d21a4
aryaman-awasthi <aryaman.awasthi79@gmail.com>
2023-05-24 18:21:24 +0530

    feat: first iteration

--- flutter-basics/road_to_devsoc/lib/main.dart
+++ flutter-basics/road_to_devsoc/lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Road to Devsoc",
       theme: ThemeData(primarySwatch: Colors.blue),
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -26,6 +27,64 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
       ),
       
+
+      body: Column(
+        children: [
+          Progress(),
+          TaskList(),
+        ]
+        ),
+    );
+  }
+}
+
+class Progress extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Text('DevSoc Approaching'),
+        LinearProgressIndicator(value: 0.0),
+      ],
+    );
+  }
+}
+
+class TaskList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          TaskItem(label: "Product Management"),
+          TaskItem(label: "Intro to HTML CSS"),
+          TaskItem(label: "ReactJS"),
+          TaskItem(label: "Design"),
+          TaskItem(label: "Flutter"),
+          TaskItem(label: "XR/AR"),
+          TaskItem(label: "Backend nodeJS"),
+          
+        ],
+      ),
+    );
+  }
+}
+
+class TaskItem extends StatelessWidget {
+
+  final String label;
+
+  const TaskItem({Key? key, required this.label}) : super (key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const Checkbox(value: false, onChanged: null),
+        Text(label)
+      ],
     );
   }
 }
